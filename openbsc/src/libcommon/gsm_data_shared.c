osmocom/openbsc

commit ec1b5a0e9e2b6549e0ede48e803095e569997355

gsm_ts2chan_nr(): add assertions for lchan_nr
Change-Id: Ibfdef347c85d4a145645a7325cd193ea1b475a54

openbsc/src/libcommon/gsm_data_shared.c
@@ -560,24 +560,38 @@ uint8_t gsm_ts2chan_nr(const struct gsm_bts_trx_ts *ts, uint8_t lchan_nr)
 	case GSM_PCHAN_TCH_F:
 	case GSM_PCHAN_PDCH:
 	case GSM_PCHAN_TCH_F_PDCH:
+		OSMO_ASSERT(lchan_nr == 0);
 		cbits = 0x01;
 		break;
 	case GSM_PCHAN_TCH_H:
+		OSMO_ASSERT(lchan_nr < 2);
 		cbits = 0x02;
 		cbits += lchan_nr;
 		break;
 	case GSM_PCHAN_CCCH_SDCCH4:
 	case GSM_PCHAN_CCCH_SDCCH4_CBCH:
+		/*
+		 * As a special hack for BCCH, lchan_nr == 4 may be passed
+		 * here. This should never be sent in an RSL message, so just
+		 * return 0. See osmo-bts-xxx/oml.c:opstart_compl().
+		 */
+		if (lchan_nr == 4) {
+			chan_nr = 0;
+			break;
+		}
+		OSMO_ASSERT(lchan_nr < 4);
 		cbits = 0x04;
 		cbits += lchan_nr;
 		break;
 	case GSM_PCHAN_SDCCH8_SACCH8C:
 	case GSM_PCHAN_SDCCH8_SACCH8C_CBCH:
+		OSMO_ASSERT(lchan_nr < 8);
 		cbits = 0x08;
 		cbits += lchan_nr;
 		break;
 	default:
 	case GSM_PCHAN_CCCH:
+		OSMO_ASSERT(lchan_nr == 0);
 		cbits = 0x10;
 		break;
 	}

osmocom/osmo-gbproxy

commit 21534045a1627608b487562879a4182ecd9f8586

gsm_pchan2chan_nr(): fix uninitialized cbits
Commit ec1b5a0e9e2b6549e0ede48e803095e569997355 introduced an unset cbits
value for the 'special hack for BCCH', where I break out of the switch
without setting cbits. Fix that.

Also remove the comment part that says 'return 0', because I don't return 0.

Change-Id: I54129d921807971eeafc23f80c57666c67b71377

openbsc/src/libcommon/gsm_data_shared.c
@@ -573,14 +573,13 @@ uint8_t gsm_pchan2chan_nr(enum gsm_phys_chan_config pchan,
 	case GSM_PCHAN_CCCH_SDCCH4_CBCH:
 		/*
 		 * As a special hack for BCCH, lchan_nr == 4 may be passed
-		 * here. This should never be sent in an RSL message, so just
-		 * return 0. See osmo-bts-xxx/oml.c:opstart_compl().
+		 * here. This should never be sent in an RSL message.
+		 * See osmo-bts-xxx/oml.c:opstart_compl().
 		 */
-		if (lchan_nr == 4) {
+		if (lchan_nr == 4)
 			chan_nr = 0;
-			break;
-		}
-		OSMO_ASSERT(lchan_nr < 4);
+		else
+			OSMO_ASSERT(lchan_nr < 4);
 		cbits = 0x04;
 		cbits += lchan_nr;
 		break;