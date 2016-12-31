osmocom/openbsc

commit 2c05f75bbf3d9a69eee1fe78eb5552acf1d7671d

global gsm_network: move allocation further up
Now that bsc_network_alloc() is separate, move it to before the VTY init (a
subsequent patch will pass the gsm_network instance as a parameter to
vty_init()).

bsc_hack.c: drop the comment that says about the VTY init: "This needs to
precede handle_options()" -- it is not accurate. Actually move the
handle_options() above both vty_init() and the bsc_network_alloc() calls, to be
able to decide which mncc callback to pass to bsc_network_alloc. It would make
sense to set this later on, but that would require further refactoring of the
bsc_network_init() and gsm_network_init() signatures, so not in this patch.

Change-Id: Ie6a7037e703b5a2d08ceeb20d35f197aaddc9d1b

openbsc/src/osmo-nitb/bsc_hack.c
@@ -270,7 +270,18 @@ int main(int argc, char **argv)
 	osmo_stats_init(tall_bsc_ctx);
 	bts_init();
 
-	/* This needs to precede handle_options() */
+	/* Parse options */
+	handle_options(argc, argv);
+
+	/* Allocate global gsm_network struct; choose socket/internal MNCC */
+	rc = bsc_network_alloc(mncc_sock_path?
+			       mncc_sock_from_cc : int_mncc_recv);
+	if (rc) {
+		fprintf(stderr, "Allocation failed. Exiting.\n");
+		exit(1);
+	}
+
+	/* Initialize VTY */
 	vty_init(&vty_info);
 	bsc_vty_init(&log_info, bsc_gsmnet);
 	ctrl_vty_init(tall_bsc_ctx);
@@ -280,25 +291,13 @@ int main(int argc, char **argv)
 		return -1;
 #endif
 
-	/* parse options */
-	handle_options(argc, argv);
-
-	/* internal MNCC handler or MNCC socket? */
-	if (mncc_sock_path) {
-		rc = bsc_network_alloc(mncc_sock_from_cc);
-		if (rc) {
-			fprintf(stderr, "Allocation failed. Exiting.\n");
-			exit(1);
-		}
+	/* Initialize MNCC socket if appropriate */
+	if (mncc_sock_path)
 		mncc_sock_init(bsc_gsmnet, mncc_sock_path);
-	} else {
+	else
 		DEBUGP(DMNCC, "Using internal MNCC handler.\n");
-		rc = bsc_network_alloc(int_mncc_recv);
-		if (rc) {
-			fprintf(stderr, "Allocation failed. Exiting.\n");
-			exit(1);
-		}
-	}
+
+	/* Read the config */
 	rc = bsc_network_configure(config_file);
 	if (rc < 0) {
 		fprintf(stderr, "Reading config failed. Exiting.\n");

openbsc/src/osmo-bsc/osmo_bsc_main.c
@@ -194,6 +194,13 @@ int main(int argc, char **argv)
 	tall_bsc_ctx = talloc_named_const(NULL, 1, "openbsc");
 	msgb_talloc_ctx_init(tall_bsc_ctx, 0);
 
+	/* Allocate global gsm_network struct */
+	rc = bsc_network_alloc(NULL);
+	if (rc) {
+		fprintf(stderr, "Allocation failed. exiting.\n");
+		exit(1);
+	}
+
 	osmo_init_logging(&log_info);
 	osmo_stats_init(tall_bsc_ctx);
 
@@ -220,11 +227,7 @@ int main(int argc, char **argv)
 	/* initialize SCCP */
 	sccp_set_log_area(DSCCP);
 
-	rc = bsc_network_alloc(NULL);
-	if (rc) {
-		fprintf(stderr, "Allocation failed. exiting.\n");
-		exit(1);
-	}
+	/* Read the config */
 	rc = bsc_network_configure(config_file);
 	if (rc < 0) {
 		fprintf(stderr, "Bootstrapping the network failed. exiting.\n");

osmocom/osmo-gbproxy

commit 8010878d87bdc7fb162247a73d4798435bef4f8b

fix 'osmo-nitb --version' segfault
Call vty_init() before handle_options() to make sure the host.app_info is
populated before --version potentially tries to print it.

The segfault was introduced by 2c05f75bbf3d9a69eee1fe78eb5552acf1d7671d in a
recent MSC-split merge.

Change-Id: Ice91256d72b9eabd52709352ba6cc6a42af2921b

openbsc/src/osmo-nitb/bsc_hack.c
@@ -269,6 +269,7 @@ int main(int argc, char **argv)
 	osmo_init_logging(&log_info);
 	osmo_stats_init(tall_bsc_ctx);
 	bts_init();
+	vty_init(&vty_info);
 
 	/* Parse options */
 	handle_options(argc, argv);
@@ -282,7 +283,6 @@ int main(int argc, char **argv)
 	}
 
 	/* Initialize VTY */
-	vty_init(&vty_info);
 	bsc_vty_init(&log_info, bsc_gsmnet);
 	ctrl_vty_init(tall_bsc_ctx);
 