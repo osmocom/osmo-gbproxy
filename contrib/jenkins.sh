383bacacf53252acc258b3b9daf2cbfcf930e7e0
Oliver Smith <osmith@sysmocom.de>
2021-02-01 10:20:52 +0100

    contrib/jenkins.sh: adjust to osmo-gbproxy split
    Related: OS#4992
    Change-Id: Ie82fc7a5a1166507460cf96ddbdf8ca77430ea64

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# jenkins build helper script for openbsc.  This is how we build on jenkins.osmocom.org
+# This is how we build on jenkins.osmocom.org
 #
 # environment variables:
-# * IU: configure 3G support (values: "--enable-iu", "--disable-iu")
 # * WITH_MANUALS: build manual PDFs if set to "1"
 # * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
 #
@@ -32,21 +31,6 @@ export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 export PATH="$inst/bin:$PATH"
 
-osmo-build-dep.sh libosmo-abis
-osmo-build-dep.sh libosmo-netif
-osmo-build-dep.sh osmo-ggsn
-osmo-build-dep.sh osmo-hlr
-
-enable_werror=""
-if [ "x$IU" = "x--enable-iu" ]; then
-    	osmo-build-dep.sh libosmo-sccp
-	osmo-build-dep.sh libasn1c
-	#osmo-build-dep.sh asn1c aper-prefix # only needed for make regen in osmo-iuh
-	osmo-build-dep.sh osmo-iuh
-else
-	enable_werror="--enable-werror"
-fi
-
 # Additional configure options and depends
 CONFIG=""
 if [ "$WITH_MANUALS" = "1" ]; then
@@ -57,18 +41,18 @@ set +x
 echo
 echo
 echo
-echo " =============================== osmo-sgsn ==============================="
+echo " =============================== osmo-gbproxy ==============================="
 echo
 set -x
 
 cd "$base"
 autoreconf --install --force
-./configure --enable-sanitize $enable_werror $IU --enable-external-tests $CONFIG
+./configure --enable-sanitize --enable-werror --enable-external-tests $CONFIG
 $MAKE $PARALLEL_MAKE
 LD_LIBRARY_PATH="$inst/lib" $MAKE check \
   || cat-testlogs.sh
 LD_LIBRARY_PATH="$inst/lib" \
-  DISTCHECK_CONFIGURE_FLAGS="$enable_werror $IU --enable-external-tests $CONFIG" \
+  DISTCHECK_CONFIGURE_FLAGS="--enable-werror --enable-external-tests $CONFIG" \
   $MAKE $PARALLEL_MAKE distcheck \
   || cat-testlogs.sh
 
